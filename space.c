5f9675a398cb2270b8658a61cb921cde5719cb8d
rubengs867 <ruben.garsan06@gmail.com>
2025-04-02 19:32:50 +0200

    space.c documentado

--- space.c
+++ space.c
@@ -22,10 +22,10 @@
  * This struct stores all the information of a space.
  */
 struct _Space {
-  Id id;                    /*!< Id number of the space, it must be unique */
-  char name[WORD_SIZE + 1]; /*!< Name of the space */
-  Set *objects;             /*!< Set of object IDs */
-  Id character_id;          /*Cada espacio solo puede tener un charcter*/
+  Id id;                    
+  char name[WORD_SIZE + 1]; 
+  Set *objects;             
+  Id character_id;          
   char gdesc[5][10];
   Bool discovered;
 };
@@ -56,7 +56,10 @@ Space *space_create(Id id) {
 }
 
 Status space_destroy(Space *space) {
-  if (!space) return ERROR;
+  if (!space){
+    return ERROR;
+  } 
+  
 
   if (space->objects) {
     set_destroy(space->objects);
@@ -68,18 +71,33 @@ Status space_destroy(Space *space) {
 
 /*Manejo del id*/
 Id space_get_id(Space *space) {
-  if (!space) return NO_ID;
+  if (!space) {
+  return NO_ID;
+  }
   return space->id;
 }
 
+Status space_set_id(Space *space, Id id){
+  if(!space || !id || id <0){
+    return ERROR;
+  }
+  space->id = id;
+  return OK;
+}
+
+
 /*Manejo de name*/
 const char *space_get_name(Space *space) {
-  if (!space) return NULL;
+  if (!space) {
+    return NULL;
+  }
   return space->name;
 }
 
 Status space_set_name(Space *space, char *name) {
-  if (!space || !name) return ERROR;
+  if (!space || !name){
+    return ERROR;
+  }
   strcpy(space->name, name);
   return OK;
 }
@@ -89,32 +107,42 @@ Id space_get_object_from_index(Space *space, int n) {
   if (!space || !space->objects) {
     return NO_ID;
   }
-  return set_get_id_from_index(space->objects, n);
+  return set_get_from_position(space->objects, n);
 }
 
 int space_get_num_objects(Space *space) {
-  if (!space || !space->objects) return 0;
-  return set_get_num_ids(space->objects);
+  if (!space || !space->objects){
+    return 0;
+  } 
+  return set_get_size(space->objects);
 }
 
 Status space_set_object(Space *space, Id object_id) {
-  if (!space || !space->objects) return ERROR;
-  return set_add_id(space->objects, object_id);
+  if (!space || !space->objects){
+    return ERROR;
+  }
+  return set_add(space->objects, object_id);
 }
 
 Status space_remove_object(Space *space, Id object_id) {
-  if (!space || !space->objects) return ERROR;
-  return set_remove_id(space->objects, object_id);
+  if (!space || !space->objects){
+    return ERROR;
+  }
+  return set_del(space->objects, object_id);
 }
 
 Bool space_has_object(Space *space, Id object_id) {
-  if (!space || !space->objects) return FALSE;
-  return set_has_id(space->objects, object_id);
+  if (!space || !space->objects){
+    return WRONG;
+  } 
+  return set_has(space->objects, object_id);
 }
 
 Status space_add_object(Space *space, Id object_id) {
-  if (!space || !space->objects) return ERROR;
-  return set_add_id(space->objects, object_id);
+  if (!space || !space->objects){
+    return ERROR;
+  }
+  return set_add(space->objects, object_id);
 }
 
 /*Manejo de character_id*/
@@ -138,7 +166,7 @@ Status space_set_character(Space *space, Id character_id) {
 
 Bool space_has_character(Space *space, Id character_id) {
   if (!space || space->character_id == NO_ID) {
-    return FALSE;
+    return WRONG;
   }
 
   if (space->character_id == character_id) {
@@ -176,8 +204,6 @@ char *space_get_i_description(Space *space, int n) {
     return space->gdesc[n];
   } else if (n == 2) {
     return "  HID    ";
-  } else {
-    return "         ";
   }
 
   return "         ";
@@ -200,7 +226,7 @@ Status space_set_description(Space *space, char description[5][10]) {
 /*Manejo de discovered*/
 Bool space_get_discovered(Space *space) {
   if (!space) {
-    return FALSE;
+    return WRONG;
   }
 
   return space->discovered;
@@ -220,26 +246,15 @@ Status space_set_discovered(Space *space, Bool discovered) {
 Status space_print(Space *space) {
   int i = 0;
   char *gdesc[10];
-
-  /* Error Control */
   if (!space) {
     return ERROR;
   }
 
-  /* 1. Print the id and the name of the space */
+
   fprintf(stdout, "--> Space (Id: %ld; Name: %s)\n", space->id, space->name);
 
-  /* 3. Print if there is an object in the space or not */
   fprintf(stdout, "---> %i object's in the space.\n", space_get_num_objects(space));
 
-  /*
-  if (space_get_object(space, object_get_id(space->objects_id[i]))) {
-    fprintf(stdout, "---> Object in the space.\n");
-  } else {
-    fprintf(stdout, "---> No object in the space.\n");
-  }*/
-
-  /* 4. Print its descrption */
   for (i = 0; i < 5; i++) {
     strcpy(gdesc[i], space_get_description(space)[i]);
     fprintf(stdout, "%s\n", gdesc[i]);
