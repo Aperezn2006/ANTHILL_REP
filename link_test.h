cd1dcb10e9e11e9b21a30148c3430776687f263a
rubengs867 <ruben.garsan06@gmail.com>
2025-04-02 16:05:50 +0200

    link test comentado

--- link_test.h
+++ link_test.h
@@ -3,204 +3,204 @@
 
 /* Manejo de creación de enlace */
 /**
- * @test Test link creation with a valid ID
- * @pre Valid ID
- * @post Non-null link pointer
+ * @test Test link con un id valido
+ * @pre ID
+ * @post devuelve un puntero valido
  */
 void test1_link_create();
 
 /**
- * @test Test link creation with an invalid ID
+ * @test Test link con id invalido
  * @pre NULL ID
- * @post Null link pointer
+ * @post debe devolver NULL
  */
 void test2_link_create();
 
 /* Manejo de ID */
 /**
- * @test Test setting a valid link ID
+ * @test Test con link id valido
  * @pre Pointer to a valid link
- * @post link_get_id() == 2
+ * @post el id que devuelve es igual al que puesto
  */
 void test1_link_set_id();
 
 /**
- * @test Test setting an invalid link ID
- * @pre Pointer to a valid link, invalid ID
- * @post ERROR returned
+ * @test Test con ID invalido
+ * @pre link creado correctamente, pero id erroneo
+ * @post devuelve ERROR
  */
 void test2_link_set_id();
 
 /**
- * @test Test getting ID from a valid link
- * @pre Pointer to a valid link
- * @post link_get_id() == 1
+ * @test Test con ID valido
+ * @pre Puntero valido
+ * @post resultado valido
  */
 void test1_link_get_id();
 
 /**
- * @test Test getting ID from a NULL link
- * @pre NULL link pointer
+ * @test Test con NULL id
+ * @pre NULL link puntero
  * @post ERROR returned
  */
 void test2_link_get_id();
 
 /* Manejo de nombre */
 /**
- * @test Test setting a valid link name
- * @pre Pointer to a valid link, valid name
+ * @test Test con valido id
+ * @pre valido puntero link y nombre
  * @post strcmp(link_get_name(), "door1") == 0
  */
 void test1_link_set_name();
 
 /**
- * @test Test setting a NULL name
- * @pre Pointer to a valid link, NULL name
- * @post ERROR returned
+ * @test Test con nombre NULL
+ * @pre Puntero valido, nombre NULL
+ * @post devuelve ERROR
  */
 void test2_link_set_name();
 
 /**
- * @test Test getting name from a valid link
- * @pre Pointer to a valid link with assigned name
- * @post Non-null name
+ * @test Test con link valido
+ * @pre Puntero valido, y nombre
+ * @post Devuelve lo que sea menos NULL
  */
 void test1_link_get_name();
 
 /**
- * @test Test getting name from a NULL link
- * @pre NULL link pointer
- * @post NULL name
+ * @test Test con puntero NULL
+ * @pre NULL link puntero
+ * @post Ndevuelve NULL
  */
 void test2_link_get_name();
 
 /* Manejo de estado de apertura */
 /**
- * @test Test setting open status to TRUE
- * @pre Pointer to a valid link
+ * @test Test con el status OPEN
+ * @pre Puntero valido
  * @post link_get_open() == TRUE
  */
 void test1_link_set_open();
 
 /**
- * @test Test setting an invalid open status
- * @pre Pointer to a valid link, invalid open status
- * @post ERROR returned
+ * @test Test con valor Bool erroneo
+ * @pre Puntero valido, pero mal el valor del Bool
+ * @post ERROR devuelto
  */
 void test2_link_set_open();
 
 /**
- * @test Test getting a valid open status
- * @pre Pointer to a valid link
- * @post link_get_open() == TRUE or FALSE
+ * @test Test con todo valido
+ * @pre Puntero valido
+ * @post link_get_open() == TRUE o FALSE
  */
 void test1_link_get_open();
 
 /**
- * @test Test getting open status from a NULL link
- * @pre NULL link pointer
+ * @test Test con link NULL
+ * @pre NULL link puntero
  * @post ERROR returned
  */
 void test2_link_get_open();
 
 /* Manejo de dirección */
 /**
- * @test Test setting a valid link direction
- * @pre Pointer to a valid link, valid direction (0-3)
- * @post link_get_direction() == direction set
+ * @test Test link valido
+ * @pre Puntero valido, direccion valida
+ * @post link_get_direction() == dierccion asiganda (0-3)
  */
 void test1_link_set_direction();
 
 /**
- * @test Test setting an invalid link direction
- * @pre Pointer to a valid link, invalid direction (>3)
- * @post ERROR returned
+ * @test Test link invalido
+ * @pre Puntero valido, direccion invalida (mas de 3 o menos de 0)
+ * @post ERROR devuelve
  */
 void test2_link_set_direction();
 
 /**
- * @test Test getting a valid link direction
- * @pre Pointer to a valid link with a set direction
- * @post link_get_direction() == valid direction (0-3)
+ * @test Test link valido
+ * @pre Puntero valido
+ * @post link_get_direction() == valida direccion (0-3)
  */
 void test1_link_get_direction();
 
 /**
- * @test Test getting direction from a NULL link
- * @pre NULL link pointer
- * @post ERROR returned
+ * @test Test link NULL
+ * @pre NULL link puntero
+ * @post ERROR devuelve
  */
 void test2_link_get_direction();
 
 /* Manejo de espacio inicial */
 /**
- * @test Test setting a valid start space for a link
- * @pre Pointer to a valid link, valid start space ID
- * @post link_get_start() == start space ID set
+ * @test Test valido todo
+ * @pre Puntero valido, id start valido
+ * @post link_get_start() == start  ID asignado
  */
 void test1_link_set_start();
 
 /**
- * @test Test setting an invalid start space
- * @pre Pointer to a valid link, invalid start space
- * @post ERROR returned
+ * @test Test link invalido
+ * @pre Puntero valido, invalido el id start
+ * @post ERROR devuelve
  */
 void test2_link_set_start();
 
 /**
- * @test Test getting a valid start space
- * @pre Pointer to a valid link with assigned start space
- * @post link_get_start() == valid start space
+ * @test Test con id valido
+ * @pre Todo valido
+ * @post link_get_start() == valido start id
  */
 void test1_link_get_start();
 
 /**
- * @test Test getting start space from a NULL link
- * @pre NULL link pointer
- * @post ERROR returned
+ * @test Test con link NULL
+ * @pre NULL link puntero
+ * @post ERROR devuelve
  */
 void test2_link_get_start();
 
 /* Manejo de espacio final */
 /**
- * @test Test setting a valid end space for a link
- * @pre Pointer to a valid link, valid end space ID
- * @post link_get_end() == end space ID set
+ * @test Test todo valido
+ * @pre Puntero valido, id end valido
+ * @post link_get_end() == end ID set
  */
 void test1_link_set_end();
 
 /**
- * @test Test setting an invalid end space
- * @pre Pointer to a valid link, invalid end space
- * @post ERROR returned
+ * @test Test id invalido
+ * @pre Puntero invalido
+ * @post ERROR devuelve
  */
 void test2_link_set_end();
 
 /**
- * @test Test getting a valid end space
- * @pre Pointer to a valid link with assigned end space
- * @post link_get_end() == valid end space
+ * @test Test todo valido
+ * @pre Puntero valido
+ * @post link_get_end() == valido end id
  */
 void test1_link_get_end();
 
 /**
- * @test Test getting end space from a NULL link
- * @pre NULL link pointer
- * @post ERROR returned
+ * @test Test con NULL link
+ * @pre NULL link puntero
+ * @post ERROR devuelve
  */
 void test2_link_get_end();
 
 /* Impresión de enlace */
 /**
- * @test Test printing a valid link
- * @pre Pointer to a valid link
+ * @test Test con link valido
+ * @pre Puntero valido
  * @post link_print() == OK
  */
 void test1_link_print();
 
 /**
- * @test Test printing a NULL link
- * @pre NULL link pointer
+ * @test Test con NULL link
+ * @pre NULL link puntero
  * @post link_print() == ERROR
  */
 void test2_link_print();
