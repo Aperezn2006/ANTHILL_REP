d284f24fc715ca6fcc9e0a3202c3e60a6f957d18
Ana O.R. <ana.olsson@estudiante.uam.es>
2025-04-28 10:13:37 +0200

    Gamemode_select

---
+++ gamemode.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# chmod -x gamemode.sh
+# ./gamemode.sh
+
+echo " ----------------------------------"
+echo "| Welcome to the Anthill game ! :] |"
+echo " ----------------------------------"
+echo "Which mode do you want to play?"
+echo "  1. Standard graphics "
+echo "  2. Fancy graphics    [!] You must have SLD2 and image.h installed"
+echo " "
+read mode
+echo " "
+
+if [ "$mode" -eq 1 ]; then
+  make clean
+  make run
+elif [ "$mode" -eq 2 ]; then
+  make clean
+  make run-sdl
+else 
+  echo "Invalid mode"
+fi
