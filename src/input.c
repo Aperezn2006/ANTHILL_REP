a90d0e6d5e6ae5352ff642378977ec74797758a4
rubengs867 <ruben.garsan06@gmail.com>
2025-04-29 15:44:36 +0200

    Update input.c

--- src/input.c
+++ src/input.c
@@ -1,7 +1,5 @@
 #include "input.h"
-
 #include <stdio.h>
-
 #include "game_actions.h"
 #include "ray.h"
 
@@ -31,67 +29,65 @@ void input_update(Game *game) {
 
       case SDL_KEYDOWN:
         if (event.key.repeat == 0) {
-          if (game_get_inventory_vis(game)) {
-            /* Menú de inventario activo: navegamos dentro de él */
-            switch (event.key.keysym.sym) {
-              case SDLK_w:
-              case SDLK_UP:
-                update_key_state(1, &game_input.inventory_up);
-                break;
-              case SDLK_s:
-              case SDLK_DOWN:
-                update_key_state(1, &game_input.inventory_down);
-                break;
-              case SDLK_g:
-                update_key_state(1, &game_input.drop);
-                break;
-              case SDLK_RETURN:
-                update_key_state(1, &game_input.inventory_confirm);
-                break;
-              case SDLK_ESCAPE:
-                update_key_state(1, &game_input.inventory_cancel);
-                break;
-              case SDLK_TAB:
-                game_toggle_inventory_vis(game);
-                break;
-            }
+          
+          if (event.key.keysym.sym == SDLK_TAB) {
+            game_toggle_inventory_vis(game); 
           } else {
-            /* Controles normales del juego */
-            switch (event.key.keysym.sym) {
-              case SDLK_w:
-                update_key_state(1, &game_input.up);
-                break;
-              case SDLK_s:
-                update_key_state(1, &game_input.down);
-                break;
-              case SDLK_a:
-                update_key_state(1, &game_input.left);
-                break;
-              case SDLK_d:
-                update_key_state(1, &game_input.right);
-                break;
-              case SDLK_ESCAPE:
-                update_key_state(1, &game_input.escape);
-                break;
-              case SDLK_e:
-                update_key_state(1, &game_input.attack);
-                break;
-              case SDLK_SPACE:
-                update_key_state(1, &game_input.jump);
-                break;
-              case SDLK_f:
-                update_key_state(1, &game_input.take);
-                break;
-              case SDLK_g:
-              case SDLK_q:
-                update_key_state(1, &game_input.inspect);
-                break;
-              case SDLK_r:
-                update_key_state(1, &game_input.use);
-                break;
-              case SDLK_TAB:
-                game_toggle_inventory_vis(game);
-                break;
+            if (game_get_inventory_vis(game)) {
+              switch (event.key.keysym.sym) {
+                case SDLK_w:
+                case SDLK_UP:
+                  update_key_state(1, &game_input.inventory_up);
+                  break;
+                case SDLK_s:
+                case SDLK_DOWN:
+                  update_key_state(1, &game_input.inventory_down);
+                  break;
+                case SDLK_g:
+                  update_key_state(1, &game_input.drop);
+                  break;
+                case SDLK_RETURN:
+                  update_key_state(1, &game_input.inventory_confirm);
+                  break;
+                case SDLK_ESCAPE:
+                  update_key_state(1, &game_input.inventory_cancel);
+                  break;
+              }
+            } else {
+              
+              switch (event.key.keysym.sym) {
+                case SDLK_w:
+                  update_key_state(1, &game_input.up);
+                  break;
+                case SDLK_s:
+                  update_key_state(1, &game_input.down);
+                  break;
+                case SDLK_a:
+                  update_key_state(1, &game_input.left);
+                  break;
+                case SDLK_d:
+                  update_key_state(1, &game_input.right);
+                  break;
+                case SDLK_ESCAPE:
+                  update_key_state(1, &game_input.escape);
+                  break;
+                case SDLK_e:
+                  update_key_state(1, &game_input.attack);
+                  break;
+                case SDLK_SPACE:
+                  update_key_state(1, &game_input.jump);
+                  break;
+                case SDLK_f:
+                  update_key_state(1, &game_input.take);
+                  break;
+                case SDLK_g:
+                case SDLK_q:
+                  update_key_state(1, &game_input.inspect);
+                  break;
+                case SDLK_r:
+                  update_key_state(1, &game_input.use);
+                  break;
+              }
             }
           }
         }
